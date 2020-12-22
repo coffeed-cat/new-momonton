11c61c61ca575f8bb01d195b3b3d6fea13e636e8
coffeed-cat <gwcattower@gmail.com>
2020-12-22 18:14:17 +0900

    Add Background

---
+++ bg.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 9;
+
+function handleImageLoad(event){
+    event.target.classList.remove("invisible");
+    event.target.classList.add("bgImage");
+}
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("invisible");
+    body.prepend(image);
+    image.addEventListener("load",handleImageLoad);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
