b89f1b28ed10111bed3daa746fb637dc9dda0b31
coffeed-cat <gwcattower@gmail.com>
2021-02-04 00:00:36 +0900

    add resizeHajime function

--- greeting.js
+++ greeting.js
@@ -1,134 +1,144 @@
 const USER_NAME = "userName";
 
-const form = document.querySelector(".js-nameForm");//폼 선택
-const input = form.querySelector("input");//input 선택
+const form = document.querySelector(".js-nameForm"); //폼 선택
+const input = form.querySelector("input"); //input 선택
 const greeting = document.querySelector(".js-greeting");
 const hajimemashite = document.querySelector(".js-hajimemashite");
 const clockBoxForGreetingJs = document.querySelector(".js-clockBox");
-const clockForGreetingJs = clockBoxForGreetingJs.querySelector(".js-clock");//삭제해도될듯
+const clockForGreetingJs = clockBoxForGreetingJs.querySelector(".js-clock"); //삭제해도될듯
 const toDoBox = document.querySelector(".js-toDoBox");
 const dummyBox = document.querySelector(".js-dummyBox");
 
-function addHoverOnRenameBtn(){
-    greetingBox.addEventListener("mouseover",function(){//////
-        const renameBtn = document.querySelector(".js-renameBtn");
-        renameBtn.style.opacity = 100;
-    });
-    greetingBox.addEventListener("mouseout",function(){///////
-        const renameBtn = document.querySelector(".js-renameBtn");
-        renameBtn.style.opacity = 0;
-    });
+function resizeHajime() {
+  input.addEventListener("input", () => {
+    hide.innerText = input.value;
+    if (hide.offsetWidth >= 300) {
+      input.style.width = hide.offsetWidth + "px";
+    }
+  });
 }
 
-function seeDummyBox(){
-    dummyBox.classList.remove("invisible");
+function addHoverOnRenameBtn() {
+  greetingBox.addEventListener("mouseover", function () {
+    //////
+    const renameBtn = document.querySelector(".js-renameBtn");
+    renameBtn.style.opacity = 100;
+  });
+  greetingBox.addEventListener("mouseout", function () {
+    ///////
+    const renameBtn = document.querySelector(".js-renameBtn");
+    renameBtn.style.opacity = 0;
+  });
 }
 
-function seeToDoBox(){
-    toDoBox.classList.remove("invisible");
+function seeDummyBox() {
+  dummyBox.classList.remove("invisible");
 }
 
-function seeClockBox(){
-    clockBoxForGreetingJs.classList.remove("invisible");
+function seeToDoBox() {
+  toDoBox.classList.remove("invisible");
 }
 
-function seeGreeting(){
-    //paintGreeting 되고나서 greeting이 보이도록 invisible 클래스를 지움
-    greeting.classList.remove("invisible");
-    addHoverOnRenameBtn();
+function seeClockBox() {
+  clockBoxForGreetingJs.classList.remove("invisible");
 }
 
-function seeAfterSubmit(){
-    hajimemashite.addEventListener("animationend",function(){
-        seeDummyBox();
-        seeClockBox();
-        seeGreeting();
-        seeToDoBox();
-        genRenameForm();
-        genBtn();
-    });
+function seeGreeting() {
+  //paintGreeting 되고나서 greeting이 보이도록 invisible 클래스를 지움
+  greeting.classList.remove("invisible");
+  addHoverOnRenameBtn();
 }
 
-function successLoad(){
-    removeForm();
-    removeHajimemashite();
+function seeAfterSubmit() {
+  hajimemashite.addEventListener("animationend", function () {
     seeDummyBox();
     seeClockBox();
     seeGreeting();
     seeToDoBox();
-    genRenameForm();//identified in rename.js
-    genBtn();//identified in rename.js
+    genRenameForm();
+    genBtn();
+  });
 }
 
-function removeForm(){
+function successLoad() {
+  removeForm();
+  removeHajimemashite();
+  seeDummyBox();
+  seeClockBox();
+  seeGreeting();
+  seeToDoBox();
+  genRenameForm(); //identified in rename.js
+  genBtn(); //identified in rename.js
+}
+
+function removeForm() {
+  form.classList.remove("showing");
+}
+
+function removeHajimemashite() {
+  hajimemashite.classList.remove("showing");
+}
+
+function removeFormAni() {
+  form.classList.add("fadeout");
+  form.removeEventListener("submit", handleSubmit); /////
+  form.addEventListener("animationend", function () {
+    form.classList.remove("fadeout");
     form.classList.remove("showing");
+  });
 }
 
-function removeHajimemashite(){
+function removeHajimemashiteAni() {
+  hajimemashite.classList.add("fadeout");
+  hajimemashite.addEventListener("animationend", function () {
+    hajimemashite.classList.remove("fadeout");
     hajimemashite.classList.remove("showing");
+  });
 }
 
-function removeFormAni(){
-    form.classList.add("fadeout");
-    form.removeEventListener("submit",handleSubmit);/////
-    form.addEventListener("animationend",function(){
-        form.classList.remove("fadeout");
-        form.classList.remove("showing");
-    });
+function saveName(userName) {
+  localStorage.setItem(USER_NAME, userName);
 }
 
-function removeHajimemashiteAni(){
-    hajimemashite.classList.add("fadeout");
-    hajimemashite.addEventListener("animationend",function(){
-        hajimemashite.classList.remove("fadeout");
-        hajimemashite.classList.remove("showing");
-    });
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  input.value = "";
+  removeHajimemashiteAni();
+  removeFormAni();
+  seeAfterSubmit();
+  paintGreeting(currentValue);
 }
 
-
-function saveName(userName){
-    localStorage.setItem(USER_NAME,userName);
+function askForName() {
+  resizeHajime();
+  form.addEventListener("submit", handleSubmit);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    saveName(currentValue);
-    input.value = '';
-    removeHajimemashiteAni();
-    removeFormAni();
-    seeAfterSubmit();
-    paintGreeting(currentValue);
+function paintGreeting(userName) {
+  const hourForGreet = new Date().getHours();
+  if (hourForGreet > 6 && hourForGreet < 12) {
+    greeting.innerText = `Good moring, ${userName}!`;
+  } else if (hourForGreet >= 12 && hourForGreet < 18) {
+    greeting.innerText = `Good afternoon, ${userName}!`;
+  } else {
+    greeting.innerText = `You must be exhausted, ${userName}`;
+  }
 }
 
-function askForName(){
-    form.addEventListener("submit",handleSubmit);
+function loadName() {
+  const userName = localStorage.getItem(USER_NAME);
+  if (userName === null) {
+    askForName();
+  } else {
+    successLoad();
+    paintGreeting(userName);
+  }
 }
 
-function paintGreeting(userName){
-    const hourForGreet = new Date().getHours();
-    if(hourForGreet > 6 && hourForGreet < 12){
-        greeting.innerText = `Good moring, ${userName}!`;
-    }else if(hourForGreet >= 12 && hourForGreet < 18){
-        greeting.innerText = `Good afternoon, ${userName}!`;
-    }else{
-        greeting.innerText = `You must be exhausted, ${userName}`;
-    }
-}
-
-
-function loadName(){
-    const userName = localStorage.getItem(USER_NAME);
-    if(userName === null){
-        askForName();
-    }else{
-        successLoad();
-        paintGreeting(userName);
-    }
-}
-
-function init(){
-    loadName();
+function init() {
+  loadName();
 }
 
 init();
